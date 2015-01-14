3cb1ab66a8fd477b429b7a9988929916b4094a7a
Ijem <ij.ofili@gmail.com>
2015-01-14 11:37:52 +0000

    setting up JS components

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+imgIjFace = new Image();
+imgIjFace.src = "images/8btf.png";
+imgIjFace.addEventListener("load", init, false);
+
+function init() {
+    
+}
