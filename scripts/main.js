9c84f989b57044e997add9e56a8163e0e0e1ee32
Ijem <ij.ofili@gmail.com>
2015-01-14 17:27:17 +0000

    added entry

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,21 @@ imgIjFace = new Image();
 imgIjFace.src = "images/8btf.png";
 imgIjFace.addEventListener("load", init, false);
 
-function init() {
+var requestAnimFrame = 
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback){
+                window.setTimeout(callback, 1000/60);
+            };
     
+function init() {
+    requestAnimFrame(update);
 }
+
+function update(){
+    context.fillRect( 10, 10, 40, 380, "#000000")
+}
+
