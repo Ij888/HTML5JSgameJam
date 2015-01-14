78f12996926165e4005a4087f7ddf2a1763b176c
Ij888 <ij.ofili@gmail.com>
2015-01-14 19:06:31 +0000

    added entry

--- scripts/main.js
+++ scripts/main.js
@@ -17,9 +17,16 @@ var requestAnimFrame =
     
 function init() {
     requestAnimFrame(update);
-}
+};
 
 function update(){
     context.fillRect( 10, 10, 40, 380, "#000000");
-}
+    context.fillRect( 10, 10, 380, 40, "#000000");
+    context.fillRect( 10, 350, 380, 40, "#000000");
+    context.fillRect( 350, 10, 40, 380, "#000000");
+    context.fillRect( 180, 10, 40, 180, "#000000");
+	
+	
+	requestAnimFrame(update);
+};
 
