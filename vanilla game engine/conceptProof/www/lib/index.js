e551c7e95ad57f4afc76d8bdc5e22babd1a6bfbc
Ij888 <ij.ofili@gmail.com>
2016-05-02 00:48:43 +0100

    added entry

---
+++ vanilla game engine/conceptProof/www/lib/index.js
@@ -0,0 +1,9 @@
+//index.js
+var score, quiz, quizAnswer, timer, option;
+var hit = "You got that right!";
+var miss = "that was a flop, try again!";
+    
+//score
+if(option == quizAnswer){
+    console.log();
+}
